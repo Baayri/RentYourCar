e26b73c839cdf9a388120c3197289088568e8431
Muhammed Bayri <m.baayri@gmail.com>
2022-03-26 15:43:43 +0300

    Initial commit

---
+++ src/main/java/hackathon/vroomfund/rentyourcar/RentYourCarApplication.java
@@ -0,0 +1,13 @@
+package hackathon.vroomfund.rentyourcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentYourCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentYourCarApplication.class, args);
+    }
+
+}
